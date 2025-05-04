ca6857327b0baec4cd679a2c0073de0ec7d99169
Melo04 <kohhmelody@gmail.com>
2025-05-04 16:44:02 +0800

    update

--- frontend/src/AddUser.jsx
+++ frontend/src/AddUser.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Webcam from 'react-webcam'
+import { Stack, HStack } from '@chakra-ui/react'
 
 const videoConstraints = {
   width: 540,
@@ -37,39 +38,43 @@ function AddUser() {
     console.log('name', name);
     console.log(url);
 
-      const response = await fetch('http://localhost:8000/user', {
-        method: 'POST',
-        body: formData,
-      });
+    const response = await fetch('http://localhost:8000/user', {
+      method: 'POST',
+      body: formData,
+    });
 
-      if (response.ok) {
-        const data = await response.json();
-        alert(`File uploaded: ${data.filename}`);
-      } else {
-        alert('Upload failed.');
-      }
+    if (response.ok) {
+      const data = await response.json();
+      alert(`File uploaded: ${data.filename}`);
+    } else {
+      alert('Upload failed.');
+    }
   }
 
   return (
-      <>
-      <Webcam
-        ref={webcamRef}
-        audio={true}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-        onUserMedia={onUserMedia}
-      />
-      
-      {url && (
-        <div>
-          <img src={url} alt="Screenshot" />
-        </div>
-      )}
-      <button onClick={capturePhoto}>Capture</button>
-      <button onClick={() => setUrl(null)}>Refresh</button>
+    <>
+      <Stack gap="4" align="flex-start">
+        <Webcam
+          ref={webcamRef}
+          audio={true}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+          onUserMedia={onUserMedia}
+        />
+
+        {url && (
+          <div>
+            <img src={url} alt="Screenshot" />
+          </div>
+        )}
+        <HStack>
+          <button onClick={capturePhoto}>Capture</button>
+          <button onClick={() => setUrl(null)}>Refresh</button>
+        </HStack>
+      </Stack>
       <form onSubmit={formSubmit}>
-        <div style={{marginBottom: 30, display: "flex", flexDirection: "column", justifyContent: "left", alignItems: "left", gap: 5}}>
-          <div style={{fontSize: 20}}>Name</div>
+        <div style={{ marginBottom: 30, display: "flex", flexDirection: "column", justifyContent: "left", alignItems: "left", gap: 5 }}>
+          <div style={{ fontSize: 20 }}>Name</div>
           <input type="text" onChange={(e) => setName(e.target.value)} />
         </div>
         <button type="submit">Register</button>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import AddUser from './AddUser.jsx';
+import { Provider } from "./components/ui/provider"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/add_user" element={<AddUser />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/add_user" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
